19caf26df54550c0a009b2a2144a377c2a699b2c
Admin <admin@utahsaa.com>
2024-05-07 17:02:30 -0600

    I starting making checkpoints and also starting making particals for when limbs fall into lava, and made cursor lock camera collisions.

--- Assets/PLAYERMOVEMENT.cs
+++ Assets/PLAYERMOVEMENT.cs
@@ -10,20 +10,27 @@ public class PLAYERMOVEMENT : MonoBehaviour
     public float speed = 10;
     public float jumpForce = 5;
     public bool canJump;
+    public Transform cam;
     // Start is called before the first frame update
     void Start()
     {
         RB = GetComponent<Rigidbody>();
-        
+        Cursor.lockState = CursorLockMode.Locked;
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 camForward = cam.forward;
+        Vector3 camRight = cam.right;
+        camForward.y = 0;
+        camRight.y = 0;
         float HV = Input.GetAxis("Horizontal");
         float VH = Input.GetAxis("Vertical");
-        Vector3 DIR = new Vector3(HV, 0, VH);
+        Vector3 forwardRelative = VH * camForward;
+        Vector3 rightRelative = HV * camRight;
+        Vector3 DIR = forwardRelative + rightRelative;
         RB.AddForce(DIR * speed * Time.deltaTime);
         if (Input.GetButtonDown("Jump")&& canJump == true){
             RB.AddForce (Vector3.up*jumpForce, ForceMode.Impulse);

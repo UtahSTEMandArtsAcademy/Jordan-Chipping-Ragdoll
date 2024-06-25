3bb808d88a443de7f696e314b97066581536361f
UtahSTEMandArtsAcademy <153949503+UtahSTEMandArtsAcademy@users.noreply.github.com>
2024-06-25 17:02:46 -0600

    Made lava rise with animation, made a level changing script and triggers for said level changer.

---
+++ Assets/TriggerEvent.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEvent : MonoBehaviour
+{
+    public UnityEvent Enter;
+    public UnityEvent Exit;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Enter.Invoke();
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Exit.Invoke();
+    }
+}
---
+++ Assets/lvl changer.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class lvlchanger : MonoBehaviour
+{
+    public vectorthreescriptableobject loca;
+    
+    public void transitioner(string name)
+    {
+        
+        SceneManager.LoadScene(name);
+
+    }
+
+    public void PlayerLocationRestartX(float X)
+    {
+        loca.location.x = X;
+    }
+    public void PlayerLocationRestartY(float Y)
+    {
+        loca.location.y = Y;
+    }
+    public void PlayerLocationRestartZ(float Z)
+    {
+        loca.location.z = Z;
+    }
+}
